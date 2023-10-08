4bd8c3b189e0c8f5588d3d7cdb44a5a8d103bd90
baselqq <baselqarb@gmail.com>
2023-10-08 13:34:54 +0200

    made token dynamic in todo list component

--- src/Todolists.js
+++ src/Todolists.js
@@ -5,7 +5,7 @@ function Todolists() {
     const [todos, setTodos] = useState([]);
     const [loading, setLoading] = useState(true);
     
-    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiZDExNzFkMzc3NzU4OWY4MDAzNWI2NzNhOWQ3OTczZGVhYmQ4NDg0ODU1ODMxYThjMjZkOTBkZWYzOTUxMGZmODM0ZDkyOWUyODhlMWVlZmYiLCJpYXQiOjE2OTU3NDgyMjQuNzY5ODY3LCJuYmYiOjE2OTU3NDgyMjQuNzY5ODY5LCJleHAiOjE3MjczNzA2MjQuNzU4NTcsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.ghcUzorRilXjpoyVPtoqW25Gsce4lhNDsHXxzouQQYFsyMFivbFs88LYonvChFQiPngnd03lRuX8YuMvJK5xXAZACKM2gaHJme7YQ1JilsG9fB5Tn6wmCo7unq-d2uU6Sa3MIsEljU8jOFJjI6eML2YDTGfCvyqA480NUG-DT99qOVHLl63MDbcc41bNwAnaCcT-JZIdTkpguiS39LwX6LJat7U6sKzqi8evOKQvVBAFMRS8BQ9Mhcrwt_BLTRrlMfIfNzoY9_eyd6TujBxQ0xFNUDT-rfeS9xo7xr5P6ItI-sL9dYKEVRC9065frisSe_fn1JDK_S9oVpQF5WgyaA2hxxvZ6AUj6flsht1ckVtxYwFARCxw0Bbdb4xAmCGNcsX2GJQ6OL_jdu0cNmbSU5lAK-KKbVjLVIV10b1ANepm3HuB52693he-J2A5hpzgUoOcp2njkT65zxaNmpg_4WyGqmWaC8BQklFX-3SP74sbnopT9QiNb-Y61xjtADd48Ke82r7J9FLU9e8rQbPyyyvjzZ-nPkSDysvioC6P3dd8O9obsSbEIowm8dSs2nvrYMb7Kbc_feiCbZDIm2eAdxH4OwyGpV2azVAtPl_bslj8w3sqJO9NHgQCHqiRAy-clSDlNwQIV-rX1GuNQZWlO45MUDwl7zIQO8vLJOhqNr0";
+    const token = localStorage.getItem('token');
 
     async function fetchTodos() {
       setLoading(false);
