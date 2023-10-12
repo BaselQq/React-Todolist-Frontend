676494a75ce2d36410d2a77e0d3ed52a292448be
baselqq <baselqarb@gmail.com>
2023-10-12 03:42:57 +0200

    added registration modal

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Form } from 'react-router-dom';
-import { Box, Button, Center, FormControl, Input, Text } from '@chakra-ui/react';
+import { Box, Button, Center, FormControl, Input, Modal, ModalBody, ModalCloseButton, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from '@chakra-ui/react';
 import Todolists from './Todolists';
+import Registration from './Registration';
 
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
+    // const [error, setError] = useState(null);
     const [user, setUser] = useState(null);
+    const { isOpen, onOpen, onClose } = useDisclosure();
     
     useEffect (()=> {
         const storedToken = localStorage.getItem('token');
@@ -20,7 +22,7 @@ function Login() {
         }
     }, []);
 
-    const handleSubmit = async (e) => {
+    const handleSignIn = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post('http://127.0.0.1:8000/api/login', {
@@ -34,7 +36,7 @@ function Login() {
                 setUser(response.data.user);
             }
         } catch(err) {
-            setError('Login Credentials missing');
+            // setError('Login Credentials missing');
         }
     }
 
@@ -55,19 +57,31 @@ function Login() {
             </div>
             ) : (
                 <div>
+                <Center>
+                    <Modal isOpen={isOpen} onClose={onClose} closeOnEsc={true}>
+                        <ModalOverlay>
+                            <ModalHeader>
+                                Registration
+                            </ModalHeader>
+                            <ModalCloseButton/>
+                            <ModalBody>
+                                <Center>
+                                    <Registration onClose={onClose}/>
+                                </Center>
+                            </ModalBody>
+                        </ModalOverlay>
+                    </Modal>
+                </Center>
                     <Center mt={30}>
                         <Box w="50%" rounded="md" p="2" bg="white">
                             <Center>
-                                <Text fontSize="5xl">Todolist</Text>
-                            </Center>
-                            <Center>
-                                <Text fontSize="5xl">Login</Text>
+                                <Text fontSize="5xl">Todolist - Login</Text>
                             </Center>
                         </Box>    
                     </Center>
                     <Center>
                         <Box boxShadow="lg" w="50%" h="350px" rounded="3em" p="10" bg="white" background='#E0E9BE'>
-                            <Form onSubmit={handleSubmit}>
+                            <Form onSubmit={handleSignIn}>
                                 <div>
                                     <FormControl>
                                     <Input 
@@ -93,9 +107,10 @@ function Login() {
                                             onChange={(e) => setPassword(e.target.value)}
                                         />
                                     </div>
-                                    {error && <p style={{color: 'red'}}>{error}</p>}
+                                    {/* {error && <p style={{color: 'red'}}>{error}</p>} */}
                                     <Center>
-                                    <Button textColor="#184E77" background="#76C893" color="blue" boxShadow="lg" rounded="2xl" mt={8} type='submit'>Sing in</Button>
+                                        <Button textColor="#184E77" background="#76C893" color="blue" boxShadow="lg" rounded="2xl" mt={8} type='submit' fontWeight="light">Sing in</Button>
+                                        <Button textColor="#184E77" background="#76C893" color="blue" boxShadow="lg" rounded="2xl" mt={8} fontWeight="light" ml={4} onClick={onOpen}>Registration</Button>
                                     </Center>
                                     </FormControl>
                                 </div>
---
+++ src/Registration.js
@@ -0,0 +1,71 @@
+import { Box, Button, Center, Input } from "@chakra-ui/react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Form } from "react-router-dom";
+
+function Registration({ onClose }) {
+
+    const [ name, setNameForRegis ] = useState('');
+    const [ email, setEmailForRegis ] = useState('');
+    const [ password, setPasswordForRegis ] = useState('');
+    const [ password_confirmation, setPasswordConfirmationForRegis ] = useState('');
+    const [returnToLogin, setReturnToLogin ] = useState(false);
+
+    async function handleRegistration() {
+        try {
+            const response = await axios.post('http://localhost:8000/api/register', {
+                name,
+                email,
+                password,
+                password_confirmation
+            });
+    
+            return response;
+        } catch (error) {
+            console.log('Error register new user', error);
+            return null;
+        }
+    }
+
+    return (
+        <Box boxShadow="lg" w="50%" h="420px" rounded="3em" p="10" bg="white" background='#E0E9BE'>
+            <Form>
+                <Input
+                    mt={6}
+                    rounded="2xl"
+                    background="white"
+                    placeholder="Name"
+                    onChange={(e) => setNameForRegis(e.target.value)}
+                />
+                <Input
+                    mt={6}
+                    rounded="2xl"
+                    background="white"
+                    placeholder="Email"
+                    type="email"
+                    onChange={(e) => setEmailForRegis(e.target.value)}
+                />
+                <Input
+                    mt={6}
+                    rounded="2xl"
+                    background="white"
+                    placeholder="Password"
+                    type="password"
+                    onChange={(e) => setPasswordForRegis(e.target.value)}
+                />
+                <Input
+                    mt={6}
+                    rounded="2xl"
+                    background="white"
+                    type="password"
+                    placeholder="Password Confirmation"
+                    onChange={(e) => setPasswordConfirmationForRegis(e.target.value)}
+                />
+                <Button textColor="#164E77" background="#76C693" color="blue" boxShadow="lg" rounded="2xl" mt={6} fontWeight="light" onClick={handleRegistration}>Confirm</Button>
+                <Button textColor="#164E77" background="#76C693" color="blue" boxShadow="lg" rounded="2xl" mt={6} fontWeight="light" ml={4} onClick={onClose}>Close</Button>
+            </Form>
+        </Box>
+    );
+}
+
+export default Registration;
